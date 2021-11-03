0f10fa6d02c64bfece92eef9cdad11015d15174e
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-04 01:34:08 +0300

    authMiddleware, checkRoleMiddleware, userController is done

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 const Router = require('express')
 const router = new Router
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
+
 
 router.post('/registration', userController.registration)
-router.post('/', userController.login)
-router.get('/auth', userController.checkAuth)
+router.post('/login', userController.login)
+router.get('/auth', authMiddleware, userController.checkAuth)
 
 
 module.exports = router
