a203cb472257ab650df99b3df431d8ea7e086735
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-03 17:31:25 +0300

    create routes, errors

---
+++ controllers/deviceController.js
@@ -0,0 +1,13 @@
+class  DeviceController {
+    async create (req, res) {
+
+    }
+    async getAll (req, res) {
+
+    }
+    async getOneDevice (req, res) {
+
+    }
+}
+
+module.exports = new DeviceController()
